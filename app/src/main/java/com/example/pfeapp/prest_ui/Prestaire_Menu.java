62775182ecfab8cf6ae5c2f0f3dbbf32e8bf4e32
Ghiles <djebaraghiles@gmail.com>
2020-03-21 16:47:52 +0100

    séparation entre client et prestataire

--- app/src/main/java/com/example/pfeapp/prest_ui/Prestaire_Menu.java
+++ app/src/main/java/com/example/pfeapp/prest_ui/Prestaire_Menu.java
@@ -1,10 +1,16 @@
 package com.example.pfeapp.prest_ui;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import com.example.pfeapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Prestaire_Menu extends AppCompatActivity {
 
@@ -12,5 +18,61 @@ public class Prestaire_Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prestaire__menu);
+
+
+
+        Toolbar toolBar = findViewById(R.id.toolBarMenu);
+        setSupportActionBar(toolBar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        BottomNavigationView BottomNav = findViewById(R.id.bottom_navigation);
+        BottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener=
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment=null;
+                    TextView titreBar=(TextView) findViewById(R.id.titreBar);
+                    switch (item.getItemId()) {
+
+                        case R.id.nav_profil_prest:
+
+
+                            titreBar.setText("Profil");
+
+
+                            break;
+
+                        case R.id.nav_messagerie_prest:
+                            titreBar.setText("Messagerie");
+
+
+                            break;
+
+                        case R.id.nav_agenda_prest:
+                            titreBar.setText("Agenda");
+
+                            break;
+
+                        case R.id.nav_stats_prest:
+                            titreBar.setText("Statistiques");
+
+                            break;
+
+                        case R.id.nav_param_prest:
+                            titreBar.setText("Paramètres");
+
+                            break;
+
+
+                    }
+
+                    //getSupportFragmentManager().beginTransaction().replace(R.id.Fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
